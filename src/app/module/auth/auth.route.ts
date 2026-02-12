9086881b8cb08eb370430111e8c47daa018cf356
safin33221 <safin33221@gmail.com>
2026-02-12 19:32:12 +0600

    add validation for create user

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller.js";
+import validateRequest from "../../middleware/validateRequest.js";
+import { authValidation } from "./auth.validation.js";
 
 
 const router: Router = Router();
@@ -7,8 +9,18 @@ const router: Router = Router();
 /**
  * Auth Routes
  */
-router.post("/register", AuthController.register);
-router.post("/login", AuthController.login);
-router.post("/refresh-token", AuthController.refreshToken);
+router.post("/register",
+    // validateRequest(authValidation.registerSchema),
+    AuthController.register);
+
+
+
+router.post("/login",
+    validateRequest(authValidation.loginSchema),
+    AuthController.login);
+
+
+router.post("/refresh-token",
+    AuthController.refreshToken);
 
 export const AuthRoutes = router;
--- src/app/module/auth/auth.validation.ts
+++ src/app/module/auth/auth.validation.ts
@@ -1,17 +1,46 @@
 import { z } from "zod";
 
-export const registerSchema = z.object({
+
+const registerSchema = z.object({
     body: z.object({
-        firstName: z.string().min(2),
-        lastName: z.string().min(2),
+        name: z
+            .string()
+            .trim()
+            .min(2, "Name must be at least 2 characters"),
+
+        email: z
+            .string()
+            .trim()
+            .toLowerCase()
+            .email("Invalid email address")
+            .max(255, "Email is too long"),
+
+        password: z
+            .string()
+            .min(6, "Password must be at least 6 characters")
+            .max(100, "Password is too long"),
+
+        phone: z
+            .string()
+            .trim()
+            .regex(
+                /^\+?[1-9]\d{7,14}$/,
+                "Phone number must be valid (8–15 digits, optional +)"
+            ),
+    }),
+});
+
+
+const loginSchema = z.object({
+    body: z.object({
         email: z.string().email(),
         password: z.string().min(6),
     }),
 });
 
-export const loginSchema = z.object({
-    body: z.object({
-        email: z.string().email(),
-        password: z.string().min(6),
-    }),
-});
+
+
+export const authValidation = {
+    registerSchema,
+    loginSchema
+}
